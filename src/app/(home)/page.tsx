0624490f7c59a80d327fb8a4e513f47bb9256060
Douglas Trindade <douglastrindade22@hotmail.com>
2024-09-12 23:36:27 -0300

    feat: categories carousel

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,12 @@
+import { Carousel } from "@/components/Carousel";
+
 const Home = () => {
   return (
-    <div className="container mt-8">
-      <span className="text-3xl font-bold pb-8">Navegue por categoria</span>
+    <div className="container h-screen mt-8">
+      <span className="text-3xl font-bold">Navegue por categoria</span>
+      <div className="mt-8">
+        <Carousel />
+      </div>
     </div>
   );
 };
