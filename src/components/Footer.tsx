0da5c9f49e1447148350587bf170589b3c8d7cc5
Douglas Trindade <douglastrindade22@hotmail.com>
2024-09-16 20:52:57 -0300

    feat: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,58 +1,76 @@
 import { Facebook, Instagram, Linkedin, Youtube } from "lucide-react";
 
 export const Footer = () => {
-  const navLinks = [
-    {
-      enterprise: [
-        { name: "Empresa" },
-        { name: "Sobre nós" },
-        { name: "Blog" },
-        { name: "Carreira" },
-      ],
-      usefulLinks: [
-        { name: "Links Úteis" },
-        { name: "Novos produtos" },
-        { name: "Produtos mais vendidos" },
-        { name: "Descontos" },
-        { name: "Carreira" },
-      ],
-    },
-  ];
+  const navLinks = {
+    company: [{ name: "Sobre nós" }, { name: "Blog" }, { name: "Carreira" }],
+    usefulLinks: [
+      { name: "Novos produtos" },
+      { name: "Produtos mais vendidos" },
+      { name: "Descontos" },
+      { name: "F.A.Q" },
+    ],
+    customerService: [
+      { name: "Contate-nos" },
+      { name: "Envio" },
+      { name: "Retorno" },
+      { name: "Rastreamento de pedidos" },
+    ],
+  };
+
   return (
-    <div className="grid grid-cols-5 ">
+    <footer className="grid grid-cols-1 md:grid-cols-4 gap-8 py-10">
       <div className="flex flex-col gap-3">
-        <span className="font-extrabold text-md">pettz.</span>
-        <span className="font-medium text-gray-600">
-          Sed viverra eget fames sit varius. Pellentesque mattis libero viverra
-          dictumst ornaresed justo convallis vitae
+        <span className="font-extrabold text-lg">
+          pettz<span className="text-primary">.</span>
         </span>
-        <span className="flex gap-2 ">
+        <p className="text-gray-600 font-medium">
+          Sed viverra eget fames sit varius. Pellentesque mattis libero viverra
+          dictumst ornare sed justo convallis vitae.
+        </p>
+        <div className="flex gap-2 mt-2">
           <Facebook className="cursor-pointer hover:text-primary" />
           <Instagram className="cursor-pointer hover:text-primary" />
           <Linkedin className="cursor-pointer hover:text-primary" />
           <Youtube className="cursor-pointer hover:text-primary" />
-        </span>
+        </div>
       </div>
-      <div className="flex flex-col items-center gap-3">
+
+      <div className="flex flex-col gap-3">
         <span className="font-bold">Empresa</span>
-        <span>Sobre nós</span>
-        <span>Blog</span>
-        <span>Carreira</span>
+        {navLinks.company.map((link, index) => (
+          <a
+            key={index}
+            className="text-gray-600 hover:text-primary cursor-pointer font-medium"
+          >
+            {link.name}
+          </a>
+        ))}
       </div>
-      <div className="flex flex-col items-center gap-3">
+
+      <div className="flex flex-col gap-3">
         <span className="font-bold">Links Úteis</span>
-        <span>Novos produtos</span>
-        <span>Produtos mais vendidos</span>
-        <span>Descontos</span>
-        <span>F.A.Q</span>
+        {navLinks.usefulLinks.map((link, index) => (
+          <a
+            key={index}
+            className="text-gray-600 hover:text-primary cursor-pointer font-medium"
+          >
+            {link.name}
+          </a>
+        ))}
       </div>
-      <div className="flex flex-col items-center gap-3">
-        <span className="font-bold">Links Úteis</span>
-        <span>Novos produtos</span>
-        <span>Produtos mais vendidos</span>
-        <span>Descontos</span>
-        <span>F.A.Q</span>
+
+      <div className="flex flex-col gap-3">
+        <span className="font-bold">Atendimento ao Cliente</span>
+        {navLinks.customerService.map((link, index) => (
+          <a
+            key={index}
+            className="text-gray-600 hover:text-primary cursor-pointer font-medium"
+          >
+            {link.name}
+          </a>
+        ))}
       </div>
-    </div>
+      <p className="text-gray-600 font-medium"> © Copyright pettz. 2024.</p>
+    </footer>
   );
 };
